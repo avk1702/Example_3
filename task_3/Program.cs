cf44b7abded3f65c2c75da6026d0263c4189b925
avk1702 <avk1702@bk.ru>
2023-02-14 20:07:22 +0500

    Кратно 7 и 23

---
+++ task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет,
+//кратно ли оно одновременно 7 и 23
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+// or int.Parse(Console.ReadLine());
+
+if (a % 7 == 0)
+{
+  if (a % 7 == 0)
+
+  {
+    Console.WriteLine("да");
+  }
+
+}
+else
+{
+  Console.WriteLine("нет");
+}
