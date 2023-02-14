66e66ef3e4b5a5f23f12ed13c262095bce025056
avk1702 <avk1702@bk.ru>
2023-02-14 19:20:59 +0500

    Кратно, не кратно

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+//то программа выводит остаток от деления.
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+// or int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+{
+  Console.WriteLine(b + " кратно " + a);
+}
+else
+{
+  int c = a % b;
+  Console.WriteLine(b + " не кратно " + a);
+  Console.WriteLine("остаток  " + c);
+}
