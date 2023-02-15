9f9e0e85512254128b5d9749a2c089d53d3d58ee
avk1702 <avk1702@bk.ru>
2023-02-15 10:24:30 +0500

    решение с использованием метода

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,19 +1,27 @@
 ﻿//Напишите программу, которая принимает на вход число и проверяет,
 //кратно ли оно одновременно 7 и 23
+// Решение с применением метода - метод назвали Check
 
-Console.WriteLine("Введите число: ");
+bool Check(int value)
+{
+  return value % 7 == 0 && value % 23 == 0;
+}
+
+Console.Write("a = ");
 int a = Convert.ToInt32(Console.ReadLine());
-// or int.Parse(Console.ReadLine());
-// лучше так if (a % 7 == 0 && a % 23 == 0)
-if (a % 7 == 0 && a % 23 == 0)
+Console.WriteLine(Check(a));
+
+// Не обязательно (для красоты)
+
+if (Check(a))
 {
   
   {
-    Console.WriteLine("да");
+    Console.WriteLine("делится на 7 и 23");
   }
 
 }
 else
 {
-  Console.WriteLine("нет");
+  Console.WriteLine("не делится на 7 и 23");
 }
