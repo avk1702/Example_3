c7df7842bfdb5ede4d37907da8081eafc4305e99
avk1702 <avk1702@bk.ru>
2023-02-15 11:40:01 +0500

    решение c применением метода

--- task_5/Program.cs
+++ task_5/Program.cs
@@ -1,16 +1,25 @@
 ﻿//Напишите программу, которая принимает на вход два числа и проверяет,
 // является ли одно число квадратом другого. 5, 25 -> да -4, 16 -> да 25, 5 -> да 8,9 -> нет
 
-// решение без применения метода
+// решение с применением метода
+bool Square(int a, int b)
+{
+  return b == a * a;
+}
+
 Console.Write(" Введите первое число  ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write(" Введите второе число  ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a*a == b)
+Console.WriteLine(Square(a,b));
+
+// Не обязательно (для красоты)
+
+if (Square(a,b))
 {
-   Console.WriteLine("Второе число является квадратом первого");
+    Console.WriteLine("Второе число является квадратом первого");  
 }
 else
 {
-  Console.WriteLine("Второе число не является квадратом первого");
+    Console.WriteLine("Второе число не является квадратом первого"); 
 }
